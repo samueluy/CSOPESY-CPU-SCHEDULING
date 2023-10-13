85993ac244bc28c4f1be05987ea2523ae504da2f
Samuel Jedidiah Uy <samueluy@Samuels-Laptop.local>
2023-10-13 09:37:02 +0800

    Added average waiting time to display & added comments to functions

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 # runner file
 
+# Import the necessary modules
 import algorithm_functions as af
 import utils as ut
 
+# Initialize an empty list to store process objects
 processes = []
 
+# Get user input for scheduling algorithm, number of processes, and process details
 scheduling_algorithm, number_of_processes, time_quantum, processes = ut.get_user_input()
 
+# Print the selected scheduling algorithm for verification
 print(scheduling_algorithm)
+
+# Run the selected scheduling algorithm based on user input
 if scheduling_algorithm == 0:
     processes = af.fcfs(processes)
 elif scheduling_algorithm == 1:
@@ -19,5 +25,8 @@ elif scheduling_algorithm == 3:
 else:
     print("Invalid input")
 
+# Display the processed output for the processes
 ut.display_output(processes)
+
+# Calculate and display the average waiting time
 ut.average_waiting_time(processes)
--- utils.py
+++ utils.py
@@ -2,21 +2,25 @@
 
 import process
 
+# Display the output in the desired format
 def display_output(processes):
-    for inst in range(len(processes)):
-        current = processes[inst]
-        print(f"{current.get_id()} start time: {current.get_start_time()} end time: {current.get_end_time()} | Waiting time: {current.get_waiting_time()}")
-        
-def average_waiting_time(processes):
-    total = 0
     for current in processes:
-        total += current.waiting_time
-    return total / len(processes)
+        print(f"{current.get_id()} start time: {current.get_start_time()} end time: {current.get_end_time()} | Waiting time: {current.get_waiting_time()}")
 
+# Calculate and display the average waiting time
+def average_waiting_time(processes):
+    total_waiting_time = 0
+    for current in processes:
+        total_waiting_time += current.get_waiting_time()
+    average_waiting_time = total_waiting_time / len(processes)
+    
+    print(f"Average waiting time: {average_waiting_time:.1f}")
+
+# Get user input for scheduling algorithm, number of processes, and process details
 def get_user_input():
     processes = []
     scheduling_algorithm, number_of_processes, time_quantum = map(int, input().split())
-    # set time_quantum = 1 if scheduling_algorithm is not RR (3)
+    # Set time_quantum = 1 if scheduling_algorithm is not RR (3)
     if scheduling_algorithm != 3:
         time_quantum = 1
     
