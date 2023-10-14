67cdb35bc540f56caecd88cb9522a51474a1a078
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-10-14 14:15:24 +0800

    implemented RR algorithm

--- algorithm_functions.py
+++ algorithm_functions.py
@@ -1,4 +1,5 @@
 import process
+from collections import deque
 
 def fcfs(processes):
     # Sort processes by arrival time
@@ -24,3 +25,46 @@ def fcfs(processes):
 # Shortest-Remaining-Time-First (SRTF)
 
 # Round-Robin (RR)
+def rr(processes, time_quantum):
+    processes.sort(key=lambda x: x.get_arrival_time())
+    current_time = processes[0].get_arrival_time()
+    ready_queue = [processes[0]]
+    remaining_time = {p.get_id(): p.get_burst_time() for p in processes}
+    has_started = {p.get_id(): False for p in processes}
+
+    while any(val > 0 for val in remaining_time.values()):
+        process_to_execute = ready_queue[0] if ready_queue else None
+
+        # If the process queue is empty, move time to the next process's arrival
+        if process_to_execute is None:
+            current_time = min(p.get_arrival_time() for p in processes if not has_started[p.get_id()])
+            process_to_execute = [p for p in processes if p.get_arrival_time() == current_time][0]
+            ready_queue.append(process_to_execute)
+
+        if not has_started[process_to_execute.get_id()]:
+            process_to_execute.set_start_time(current_time)
+            has_started[process_to_execute.get_id()] = True
+
+        # Calculating end of execution for current quantum slice
+        execution_time = min(remaining_time[process_to_execute.get_id()], time_quantum)
+        current_time += execution_time
+        remaining_time[process_to_execute.get_id()] -= execution_time
+        
+        # Updating end time and other time metrics if process is finished
+        if remaining_time[process_to_execute.get_id()] == 0:
+            process_to_execute.set_end_time(current_time)
+            process_to_execute.set_turn_around_time(current_time - process_to_execute.get_arrival_time())
+            process_to_execute.set_waiting_time(process_to_execute.get_turn_around_time() - process_to_execute.get_burst_time())
+        
+        # Adding processes that have arrived to ready_queue and removing completed processes
+        ready_queue.extend(
+            p for p in processes 
+            if p.get_arrival_time() <= current_time and not has_started[p.get_id()] and p not in ready_queue
+        )
+        ready_queue = [p for p in ready_queue if remaining_time[p.get_id()] > 0]
+        
+        # Ensuring cyclic execution of processes in ready_queue
+        if ready_queue and process_to_execute == ready_queue[0]:
+            ready_queue.append(ready_queue.pop(0))
+            
+    return processes
