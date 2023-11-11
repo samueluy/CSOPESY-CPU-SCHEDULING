7601fbd7ba56c1c9ed8b5a911afbe5c6b7640606
John Carlo <john_carlo_joyo@dlsu.edu.ph>
2023-11-11 20:11:11 +0800

    removed cumulative waiting time, avg wait time is now rounded off to 2 decimals, added validation for no. of processes and time quantum

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # utility functions for the project
 
+import sys
 import process
 
 # Display the output in the desired format
@@ -16,22 +17,34 @@ def display_output(processes):
         for i in range(1, len(current.start_times)):
             waiting_time_since_last_cycle = current.start_times[i] - current.end_times[i-1]
             total_waiting_time += waiting_time_since_last_cycle
-            print(f"{current.get_id()} start time: {current.start_times[i]} end time: {current.end_times[i]} | Waiting time: {total_waiting_time}")
+            print(f"{current.get_id()} start time: {current.start_times[i]} end time: {current.end_times[i]} | Waiting time: {waiting_time_since_last_cycle}")
 def average_waiting_time(processes):
     total_waiting_time = 0
     for current in processes:
         total_waiting_time += current.get_waiting_time()
     average_waiting_time = total_waiting_time / len(processes)
-    
-    print(f"Average waiting time: {average_waiting_time}")
+
+    print(f"Average waiting time: {average_waiting_time:.2f}")
+
 
 # Get user input for scheduling algorithm, number of processes, and process details
 def get_user_input():
     processes = []
     scheduling_algorithm, number_of_processes, time_quantum = map(int, input().split())
+
+    # Validate number of processes
+    if number_of_processes < 3 or number_of_processes > 100:
+        print("Invalid number of processes. Enter a value between 3 and 100.")
+        sys.exit("Exiting due to invalid number of processes.")
+
     # Set time_quantum = 1 if scheduling_algorithm is not RR (3)
     if scheduling_algorithm != 3:
         time_quantum = 1
+    else:
+        # Validate time quantum only if the algorithm is Round Robin
+        if time_quantum < 1 or time_quantum > 100:
+            print("Invalid time quantum. Enter a value between 1 and 100.")
+            sys.exit("Exiting due to invalid time quantum.")
     
     for inst in range(number_of_processes):
         id, arrival_time, burst_time = map(int, input().split())
